649edb478ed99ee513315a4e2a414a471b6eea5d
Mike Turquette <mturquette@ti.com>
2011-05-17 09:43:09 -0500

    cpufreq: introduce hotplug governor
    The "hotplug" governor scales CPU frequency based on load, similar to
    "ondemand".  It scales up to the highest frequency when "up_threshold"
    is crossed and scales down one frequency at a time when "down_threshold"
    is crossed.  Unlike those governors, target frequencies are determined
    by directly accessing the CPUfreq frequency table, instead of taking
    some percentage of maximum available frequency.
    
    The key difference in the "hotplug" governor is that it will disable
    auxillary CPUs when the system is very idle, and enable them again once
    the system becomes busy.  This is achieved by averaging load over
    multiple sampling periods; if CPUs were online or offlined based on a
    single sampling period then thrashing will occur.
    
    Sysfs entries exist for "hotplug_in_sampling_periods" and for
    "hotplug_out_sampling_periods" which determine how many consecutive
    periods get averaged to determine if auxillery CPUs should be onlined or
    offlined.  Defaults are 5 periods and 20 periods respectively.
    Otherwise the standard sysfs entries you might find for "ondemand" and
    "conservative" governors are there.
    
    To use this governor it is assumed that your CPUfreq driver has
    populated the CPUfreq table, CONFIG_NO_HZ is enabled and
    CONFIG_HOTPLUG_CPU is enabled.
    
    Changes in V2:
    	Corrected default sampling periods
    	Optimized load history array resizing
    	Maintain load history when resizing array
    	Add locking to dbs_check_cpu
    	Switch from enable_nonboot_cpus to cpu_up
    	Switch from disable_nonboot_cpus to down_cpu
    	Fix some printks
    	Coding style around for-loops
    
    Signed-off-by: Mike Turquette <mturquette@ti.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -375,6 +375,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
+extern struct cpufreq_governor cpufreq_gov_hotplug;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
 #endif
 
 /* the following are for use in governors, or anywhere else */
